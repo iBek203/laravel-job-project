80a64336edc0f527e056c499ba78870a89acc892
Oybek <62173911+iBek203@users.noreply.github.com>
2023-04-02 18:13:43 +0500

    project

---
+++ routes/web.php
@@ -0,0 +1,57 @@
+<?php
+
+use App\Http\Controllers\ListingController;
+use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Models\Listing;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// All listings
+Route::get('/', [ListingController::class, 'index']);
+
+// Show create form
+Route::get('/listings/create', [ListingController::class, 'create'])->middleware('auth');
+
+// Store listing data
+Route::post('/listings',[ListingController::class, 'store'])->middleware('auth');
+
+// Show edit form
+Route::get('/listings/{listing}/edit', [ListingController::class, 'edit'])->middleware('auth');
+
+// Update listing
+Route::put('/listings/{listing}', [ListingController::class, 'update'])->middleware('auth');
+
+// Delete listing
+Route::delete('/listings/{listing}', [ListingController::class, 'destroy'])->middleware('auth');
+
+// Manage listings
+Route::get('/listings/manage', [ListingController::class, 'manage'])->middleware('auth');
+
+// Single listing
+Route::get('/listings/{listing}', [ListingController::class, 'show']);
+
+// Show Register/Create form
+Route::get('/register',[UserController::class, 'create'])->middleware('guest');
+
+// Create new user
+Route::post('/users',[UserController::class, 'store']);
+
+// Log user out
+Route::post('/logout', [UserController::class, 'logout'])->middleware('auth');
+
+// Show login form
+Route::get('/login', [UserController::class, 'login'])->name('login')->middleware('guest');
+
+// Log in user
+Route::post('/users/authenticate', [UserController::class, 'authenticate']);
